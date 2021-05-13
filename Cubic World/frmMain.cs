de684486de8f7d175006c6cce24dd937d0e604ae
NucleusGames <84132777+NucleusGames@users.noreply.github.com>
2021-05-13 18:04:56 +0200

    Initialize Triangle

--- Cubic World/frmMain.cs
+++ Cubic World/frmMain.cs
@@ -9,12 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Graphics.ES30;
 using OpenTK.Input;
 
 namespace Cubic_World
 {
     public class Game : GameWindow
     {
+        Shader shader;
+
+        int VertexArrayObject;
         public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) 
         {
             
@@ -22,6 +26,32 @@ namespace Cubic_World
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
+            float[] vertices = {
+                -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+                 0.5f, -0.5f, 0.0f, //Bottom-right vertex
+                 0.0f,  0.5f, 0.0f  //Top vertex
+            };
+
+            VertexArrayObject = GL.GenVertexArray();
+
+            // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+            // 1. bind Vertex Array Object
+            GL.BindVertexArray(VertexArrayObject);
+            // 2. copy our vertices array in a buffer for OpenGL to use
+            //GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            // 3. then set our vertex attributes pointers
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.UseProgram();
+            GL.BindVertexArray(VertexArrayObject);
+            someOpenGLFunctionThatDrawsOurTriangle();
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
             KeyboardState input = Keyboard.GetState();
 
             if (input.IsKeyDown(Key.Escape))
@@ -30,6 +60,42 @@ namespace Cubic_World
             }
 
             base.OnUpdateFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //Code goes here
+            shader = new Shader("shader.vert", "shader.frag");
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            //GL.DeleteBuffer(VertexBufferObject);
+
+            shader.Dispose();
+
+            base.OnUnload(e);
         }
     }
 }
