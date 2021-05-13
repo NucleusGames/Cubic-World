53704416b3a2334b22f0cbe78a25234d8e4de2bc
Marc Peters <marc.peters2000@outlook.de>
2021-05-13 14:23:30 +0200

    Test

--- Cubic World/Form1.Designer.cs
+++ Cubic World/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Cubic_World
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -29,10 +29,17 @@ namespace Cubic_World
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Cubic World/Program.cs
+++ Cubic World/Program.cs
@@ -16,7 +16,7 @@ namespace Cubic_World
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
